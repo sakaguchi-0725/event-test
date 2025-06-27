213f4949c6496258c605a0d7e886c2cb8a876326
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-06-27 10:50:00 +0900

    onClickイベント追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,10 @@ export default function Home() {
     setEventType('touch end')
   }
 
+  const handleClick = () => {
+    setEventType('click')
+  }
+
   return (
     <div className="flex flex-col gap-10 items-center justify-center h-screen">
       <h1 className="text-2xl font-bold md:text-4xl">電話番号長押しイベント調査用</h1>
@@ -43,6 +47,16 @@ export default function Home() {
             className="text-xl font-bold px-4 py-2 bg-blue-500 text-white rounded-md"
             onTouchStart={handleTouchStart}
             onTouchEnd={handleTouchEnd}
+          >
+            電話
+          </a>
+        </div>
+        <div className="flex flex-col items-center gap-2 justify-center">
+          <h2 className="text-xl font-bold md:text-2xl">Click Event</h2>
+          <a
+            href="tel:0312345678"
+            className="text-xl font-bold px-4 py-2 bg-blue-500 text-white rounded-md"
+            onClick={handleClick}
           >
             電話
           </a>
